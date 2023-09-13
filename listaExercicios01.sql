6811f0253f3ac6a304896044c773ea2e09196e3b
rafa-gomes-2007 <102773088+rafa-gomes-2007@users.noreply.github.com>
2023-09-12 23:46:41 -0300

    commit 4

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -3,3 +3,8 @@ select titulo from livros;
 select nome from autores where nascimento < "1900-01-01"; 
 
 select titulo from livros where autor_id = "1";
+
+select a.nome,m.curso 
+from alunos as a
+inner join matriculas as m on m.aluno_id = a.id 
+where m.curso = "Engenharia de Software";
